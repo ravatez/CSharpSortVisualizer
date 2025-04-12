4de7dcc747bef7993208d48cd3082c31df8ba630
Vishrut Sharma <vishrutsharma1994@gmail.com>
2025-04-12 15:22:34 +0530

    Implemented Generic Base class for all Sorting Algos

--- Base.cs
+++ Base.cs
@@ -1,4 +1,36 @@
-interface ISort
+using System;
+using System.Linq;
+
+namespace Sort.Base
 {
-    public void Sort(T[] array);
+    public abstract class Base
+    {
+        protected int[] elements;
+        public virtual void Sort(); 
+        public bool IsEmpty() => elements.Length <= 0;
+
+        protected void Swap(int swapIndexA,int swapIndexB)
+        {
+            if(swapIndexA <0 || swapIndexA >= elements.Length || swapIndexB <0 || swapIndexB >= elements.Length)// Bounds checking
+            {
+                Console.WriteLine("Swap Index not in bounds");
+                return;
+            }
+
+            int temp = elements[swapIndexA];
+            elements[swapIndexA] = elements[swapIndexB];
+            elements[swapIndexB] = temp;
+        }  
+
+        public void Display()
+        {
+            if(IsEmpty())
+            {
+                Console.WriteLine("Array is Empty");
+                return;
+            }
+
+            elements.ToList().ForEach(n=> Console.WriteLine(n));
+        }     
+    }
 }
--- BubbleSort.cs
+++ BubbleSort.cs
@@ -1,35 +1,32 @@
 using System;
+using Sort.Base;
 
-public class BubbleSort : IsSort
+public class BubbleSort : Base
 {
-    public void Sort(int[] array)
+    public void Sort()
     {
-        int n = array.Length;
-        bool swapped;
+        if(IsEmpty())
+        {
+            Console.WriteLine("Doesnt have elements to Sort");
+            return;
+        }
 
+        int n = elements;
+        bool swapped;
         for (int i = 0; i < n - 1; i++)
         {
             swapped = false;
-
             for (int j = 0; j < n - 1 - i; j++)
             {
-                if (array[j] > array[j + 1])
+                if (elements[j] > elements[j + 1])
                 {
-                    Swap(array, j, j + 1);
+                    Swap(j, j + 1);
                     swapped = true;
                 }
             }
-
             // If no two elements were swapped in the inner loop, then the array is already sorted
             if (!swapped)
                 break;
         }
-    }
-
-    private static void Swap(int[] array, int a, int b)
-    {
-        int temp = array[a];
-        array[a] = array[b];
-        array[b] = temp;
     }
 }
